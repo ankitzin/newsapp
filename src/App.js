c03eeced50a4e55bc689ca8a264c26aba253387d
ankitzin <ankit.zin279@gmail.com>
2022-10-08 08:50:38 +0530

    Added top loading bar

--- src/App.js
+++ src/App.js
@@ -8,24 +8,39 @@ import {
 import React, { Component } from 'react'
 import NavBar from './components/NavBar';
 import News from './components/News';
+import LoadingBar from 'react-top-loading-bar'
+
 
 export default class App extends Component {
   country = 'in'
   pageSize = 9
+  
+  state = {
+    progress:0
+  }
+
+  setProgress=(progress)=>{
+    this.setState({progress:progress})
+  }
   render() {
     return (
       <>
       <Router>
       <NavBar />
+      <LoadingBar
+        color='#f11946'
+        progress={this.state.progress}
+        onLoaderFinished={() => this.setProgress(this.state.progress)}
+      />
         <Routes>
-              <Route exact path="/business" element={ <News key="business" pageSize={this.pageSize} country={this.country} category='business'></News>} />
-              <Route exact path="/" element={ <News key="general" pageSize={this.pageSize} country={this.country} category='general'></News>} />
-              <Route exact path="/entertainment" element={ <News key="entertainment" pageSize={this.pageSize} country={this.country} category='entertainment'></News>} />
-              <Route exact path="/general" element={ <News key="general" pageSize={this.pageSize} country={this.country} category='general'></News>} />
-              <Route exact path="/health" element={ <News key="health" pageSize={this.pageSize} country={this.country} category='health'></News>} />
-              <Route exact path="/science" element={ <News key="science" pageSize={this.pageSize} country={this.country} category='science'></News>} />
-              <Route exact path="/technology" element={ <News key="technology" pageSize={this.pageSize} country={this.country} category='technology'></News>} />
-              <Route exact path="/sports" element={ <News key="sports" pageSize={this.pageSize} country={this.country} category='sports'></News>} />
+              <Route exact path="/business" element={ <News setProgress={this.setProgress}  key="business" pageSize={this.pageSize} country={this.country} category='business'></News>} />
+              <Route exact path="/" element={ <News setProgress={this.setProgress}  key="general" pageSize={this.pageSize} country={this.country} category='general'></News>} />
+              <Route exact path="/entertainment" element={ <News setProgress={this.setProgress}  key="entertainment" pageSize={this.pageSize} country={this.country} category='entertainment'></News>} />
+              <Route exact path="/general" element={ <News setProgress={this.setProgress}  key="general" pageSize={this.pageSize} country={this.country} category='general'></News>} />
+              <Route exact path="/health" element={ <News setProgress={this.setProgress}  key="health" pageSize={this.pageSize} country={this.country} category='health'></News>} />
+              <Route exact path="/science" element={ <News setProgress={this.setProgress}  key="science" pageSize={this.pageSize} country={this.country} category='science'></News>} />
+              <Route exact path="/technology" element={ <News setProgress={this.setProgress}  key="technology" pageSize={this.pageSize} country={this.country} category='technology'></News>} />
+              <Route exact path="/sports" element={ <News setProgress={this.setProgress}  key="sports" pageSize={this.pageSize} country={this.country} category='sports'></News>} />
         </Routes>
       </Router>
        
--- src/components/News.js
+++ src/components/News.js
@@ -31,6 +31,7 @@ export class News extends Component {
     }
 
     async updateNews() {
+        this.props.setProgress(0)
         const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=d093053d72bc40248998159804e0e67d&page=${this.state.page}&pageSize=${this.props.pageSize}`;
         this.setState({ loading: true });
         let data = await fetch(url);
@@ -40,6 +41,7 @@ export class News extends Component {
             totalResults: parsedData.totalResults,
             loading: false, 
         })
+        this.props.setProgress(100)
 
     }
     async componentDidMount() {
